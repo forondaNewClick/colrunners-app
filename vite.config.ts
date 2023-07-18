1c13ba5858fb652d0742bf61757d7839087dac1f
Jonathan Madrid <brianforonda94@gmail.com>
2023-07-18 12:15:59 -0500

    proyecto colrunners-app

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      manifest: {
+        name: 'ColRunners',
+        short_name: 'ColR',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
